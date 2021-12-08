f52425963e4a06fa943b1158f4c0e61e33988f8e
paweu-gith <64502920+paweu-gith@users.noreply.github.com>
2021-12-08 14:49:09 +0100

    test

--- src/main/java/com/cinema/CinemaProject/CinemaProjectApplication.java
+++ src/main/java/com/cinema/CinemaProject/CinemaProjectApplication.java
@@ -8,7 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CinemaProjectApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(CinemaProjectApplication.class, args);
+		
 	}
 	
 }
